ef1fe7a237fdc2952e2cb73cfe1e46f170f759f2
Feynman <2901954062@qq.com>
2022-12-14 11:38:41 +0800

    workflow: 项目结构修改

--- basic_app/src/main.ts
+++ basic_app/src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './samples/node-api'
 
-import router from './router'
-import store from './store'
 // 国际化
 import ViewUIPlus from 'view-ui-plus';
 import i18n from '@/locale'
@@ -15,14 +13,14 @@ import "@/styles/index"
 import config from '@/config'
 import axios from './libs/axiosTool'
 
-import cascaderMulti from "cascader-multi";
-
 const app = createApp(App);
+
 
 app.use(i18n)
 app.use(ViewUIPlus, {
   i18n
 })
+app.config.globalProperties.$config = config;
 
 app.component('Button', Button);
 app.component('Input', Input);
---
+++ basic_app/src/styles/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: fg
+ * @Date: 2022-12-13 16:21:17
+ * @LastEditors: fg
+ * @LastEditTime: 2022-12-13 16:21:18
+ * @Description: style 主入口
+ */
+import 'view-ui-plus/dist/styles/viewuiplus.css'
+import "./global.less"
+import "./default.less"
