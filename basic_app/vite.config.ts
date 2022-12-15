855823cb712cf773b31b2b3719b42b33eb632306
Feynman <2901954062@qq.com>
2022-12-15 13:31:19 +0800

    workflow: 窗口不同分辨率适配问题取消rem

--- basic_app/vite.config.ts
+++ basic_app/vite.config.ts
@@ -70,12 +70,12 @@ export default defineConfig({
         autoprefixer({
           overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8'],
         }),
-        postCssPxToRem({
+        /* postCssPxToRem({
           // 自适应，px>rem转换
           rootValue: 102.4, // 75表示750设计稿(1rem = 75px)，37.5表示375设计稿  
           propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
           selectorBlackList: ['norem'], // 过滤掉norem-开头的class，不进行rem转换
-        }),
+        }), */
       ],
     },
   },
