d7329c89df884bc22b0aa7d9364726b9390881c6
Feynman <2901954062@qq.com>
2023-01-09 18:13:37 +0800

    feat: 会议纪要弹窗页面结构搭建

--- basic_app/src/views/meeting/index.vue
+++ basic_app/src/views/meeting/index.vue
@@ -2,7 +2,7 @@
  * @Author: fg
  * @Date: 2022-12-15 16:22:27
  * @LastEditors: fg
- * @LastEditTime: 2023-01-06 16:44:27
+ * @LastEditTime: 2023-01-09 16:08:36
  * @Description: content
 -->
 <template>
@@ -246,6 +246,7 @@ onMounted(() => {
 onUnmounted(() => {
   // 取消防抖
   watchSearchForm.cancel();
+  clearTimeout();
 });
 /* onUpdated(() => {
   console.log("onUpdated");
--- basic_app/src/views/meeting/summary.vue
+++ basic_app/src/views/meeting/summary.vue
@@ -2,19 +2,50 @@
  * @Author: fg
  * @Date: 2023-01-09 10:39:59
  * @LastEditors: fg
- * @LastEditTime: 2023-01-09 10:40:01
+ * @LastEditTime: 2023-01-09 17:42:33
  * @Description: content
 -->
 <template>
-  <div class="equipment">
-    <h1>hello model</h1>
+  <div class="summary">
+    <div class="header f-row-b-c">
+      <h1>会议纪要</h1>
+      <SystemOpt
+        :isShowChangeSize="false"
+        color="var(--bg)"
+        :isShowAllSize="false"
+      ></SystemOpt>
+    </div>
+    <div class="content"></div>
+    <div class="footer"></div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SystemOpt from "@/commons/system_opt/index";
+</script>
 <style scoped lang="less">
-.equipment {
-  h1 {
-    font-size: 40px;
+.summary {
+  .size(100vw,100vh);
+
+  .header {
+    .size(100%,48px);
+    padding: 0 8px 0 16px;
+    box-sizing: border-box;
+    background-color: @f_color_active;
+    -webkit-app-region: drag;
+    h1 {
+      font-size: 16px;
+      color: @bg;
+    }
+  }
+  .content {
+    width: 100%;
+    height: calc(100% - 121px);
+    padding: 20px;
+    box-sizing: border-box;
+  }
+  .footer {
+    .size(100%, 73px);
+    border-top: 1px solid @meet_summary_bbc;
   }
 }
 </style>
